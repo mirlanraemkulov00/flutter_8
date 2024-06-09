3399fe3d02deee3fe6481fcac5f83d2322426979
mirlanraemkulov00 <strategarxy@gmail.com>
2024-06-09 20:49:47 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,115 +11,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Screen1(),
+      home: Screen4(),
     );
   }
 }
 
-class Screen1 extends StatelessWidget {
-  const Screen1({super.key});
+class Screen4 extends StatefulWidget {
+  const Screen4({super.key});
+
+  @override
+  State<Screen4> createState() => _Screen4State();
+}
+
+class _Screen4State extends State<Screen4> {
+  var num1, num2, sum;
+
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
+  
+  void add() {
+    num1 = int.parse(controller1.text);
+    num2 = int.parse(controller2.text);
+
+    setState(() {
+      sum = num1 + num2;
+    });
+  }
+
+  void sub() {
+    num1 = int.parse(controller1.text);
+    num2 = int.parse(controller2.text);
+
+    setState(
+      () {
+        sum = num1 - num2;
+      },
+    );
+  }
+
+  void mul() {
+    num1 = int.parse(controller1.text);
+    num2 = int.parse(controller2.text);
+
+    setState(() {
+      sum =  num1 / num2;
+    });
+  }
+
+  void div() {
+    num1 = int.parse(controller1.text);
+    num2 = int.parse(controller2.text);
+
+    setState(() {
+      sum =  num1 * num2;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:  Color(0xff131921),
-        leading: const Icon(Icons.arrow_back),
+        backgroundColor: Colors.blue,
+        title: Text('Calculator App'),
         centerTitle: true,
-        title: Column(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
           children: [
-            Text(
-              'Москва в кино',
-              style: TextStyle(fontSize: 10),
-              selectionColor: Color(0xffFFFFFF),
+            
+            TextField(
+              decoration: InputDecoration(hintText: 'Введите число 1'),
+              controller: controller1,
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Введите число 1'),
+              controller: controller2,
+              keyboardType: TextInputType.number,
+            ),
+            ElevatedButton(
+              onPressed: add,
+              child: Text('+'),
+            ),
+            ElevatedButton(
+              onPressed: sub,
+              child: Text('-'),
+            ),
+            ElevatedButton(
+              onPressed: mul,
+              child: Text('/'),
+            ),
+            ElevatedButton(
+              onPressed: div,
+              child: Text('*'),
             ),
             Text(
-              'Москва Марины Цветаевой',
-              style: TextStyle(fontSize: 10),
-              selectionColor: Color(0xffFFFFFF99),
+              ''' Ответ = ${sum == null ? 0 : sum} ''',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
             ),
           ],
         ),
-        actions: const [
-          Icon(Icons.clear),
-          SizedBox(width: 20),
-        ],
-      ),
-      body: ListView(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 175,
-                height: 4,
-                color: const Color(0xff64BDFF),
-              ),
-            ],
-          ),
-          Row(
-            children:  [
-              Icon(Icons.question_mark),
-              SizedBox(width: 8),
-              Text('1/10'),
-            ],
-          ),
-          Expanded(
-            child: Image.asset(
-              'assets/images/image.png',
-            ),
-          ),
-          Expanded(
-            child: const Text(
-              'С момента выхода на экраны советских кинотеатров фильма «Служебный роман» прошло уже более 40 лет. Картина моментально завоевала сердца публики и стала одной из самых любимых отечественных комедий.',
-              style: TextStyle(fontSize: 14),
-            ),
-          ),
-          Expanded(
-            child: const Text(
-              'В Москве на улице Пятницкой находится павильон метро, размещенный на месте снесенной церкви. Напишите название церкви. Подсказкой станет стена-граффити дома, стоящего прямо у выхода метро «Новокузнецкая».',
-              style: TextStyle(fontSize: 14),
-            ),
-          ),
-          Icon(Icons.circle_sharp,  ),
-          Expanded(child: Text('Наш директор',),),
-          Icon(Icons.circle_outlined),
-          Expanded(child: Text('Наша мымра'),),
-          Icon(Icons.circle_outlined),
-          Expanded(child: Text('Мужчина в юбке', ),),
-          Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 80,
-                  color: Colors.red,
-                  child: const Text('Ответить'),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text('Далее'),
-                        SizedBox(width: 10),
-                        Icon(Icons.arrow_forward_sharp),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
+
+  
 }
